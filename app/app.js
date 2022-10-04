f516bccd9b4f0fed9cb852da12e85bb5813d2019
daniilv-me <me@daniilv.me>
2022-10-03 21:03:59 -0400

    Final code

--- app/app.js
+++ app/app.js
@@ -17,7 +17,8 @@ import { MongoURI, Secret } from '../config/config.js';
 
 
 // Import Routes
-import indexRouter from './routes/index.route.server.js'
+import indexRouter from './routes/index.route.server.js';
+import movieRouter from './routes/movies.route.server.js';
 
 // Instantiate Express Application
 const app = express();
@@ -52,6 +53,7 @@ app.use(session({
 
 //Use Routes
 app.use('/', indexRouter);
+app.use('/', movieRouter);
 
 export default app;
 
